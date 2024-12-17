bb41900cc3638416cdb94848229a4fb97f8f7cdd
carlosrrdev <rr.carlos.dev@gmail.com>
2024-12-17 08:45:09 -0600

    fix errors in paths

--- gen.sh
+++ gen.sh
@@ -3,15 +3,15 @@
 # Look for .md files in the notes directory
 
 # Use mapfile to read .md file paths into an array
-md_files=($(find ./notes -type f -name "*.md"))
+md_files=($(find ./public/notes -type f -name "*.md"))
 
 # Create an array of paths in JSON format
 paths_json=$(printf '%s\n' "${md_files[@]}" | jq -R . | jq -s .)
 
 # Remove old paths.json file if it exists
-rm -f ./paths.json
+rm -f ./public/paths.json
 
 # Write the JSON array to paths.json in the root directory
-echo "$paths_json" > ./paths.json
+echo "$paths_json" > ./public/paths.json
 
 echo "paths.json has been created with the list of .md file paths."
--- notes/hello-world.md
+++
@@ -1,3 +0,0 @@
-# This file was created in GitHub!
-
-Yes sir, automation is pretty cool!
